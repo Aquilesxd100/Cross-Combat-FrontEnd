350861787aaea88a17b41200c48c378f5faaa515
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-03-06 01:51:30 -0300

    adicao de arquivos + padrao css global

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Rotas from './components/rotas/Rotas';
+import "./styles/global.css";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
