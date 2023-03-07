89994f960b179330c41630f6b27718e0b39251ec
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-03-07 19:38:06 -0300

    organização do visual da Tela Inicial

--- src/pages/telaInicial/TelaInicial.tsx
+++ src/pages/telaInicial/TelaInicial.tsx
@@ -1,12 +1,16 @@
 import imgLogo from "../../resources/images/logo.png";
+import botaoMusicaON from "../../resources/images/icone-som.png";
+import botaoMusicaOFF from "../../resources/images/icone-som-desligado.png";
 function TelaInicial() {
     return (
-        <div className="h-full w-full p-2.5 flex flex-col">
-            <header className="bg-red-50 h-3/6 w-full">
-                <img src={imgLogo}/>
+        <div className="h-full w-full px-2.5 pb-2.5 flex flex-col">
+            <header className="bg-red-50 h-3/6 w-full flex justify-center relative">
+                <button style={{backgroundImage:`url(${botaoMusicaON}`}} className="h-14 w-14 absolute left-1 top-3 bg-contain" />
+                <img src={imgLogo} className="h-6/7 -top-4 relative"/>
             </header>
             <main className="bg-blue-50 h-3/6 w-full">
-                
+                <button></button>
+                <button></button>
             </main>
         </div>
     );
