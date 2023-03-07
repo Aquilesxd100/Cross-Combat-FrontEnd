9573921128083388209c075b12067631d89eb6c3
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-03-07 18:59:22 -0300

    Adição de componentes na TelaInicial + img

--- src/pages/telaInicial/TelaInicial.tsx
+++ src/pages/telaInicial/TelaInicial.tsx
@@ -1,9 +1,13 @@
+import imgLogo from "../../resources/images/logo.png";
 function TelaInicial() {
     return (
-        <div>
-            <header>
-                
+        <div className="h-full w-full p-2.5 flex flex-col">
+            <header className="bg-red-50 h-3/6 w-full">
+                <img src={imgLogo}/>
             </header>
+            <main className="bg-blue-50 h-3/6 w-full">
+                
+            </main>
         </div>
     );
 }
