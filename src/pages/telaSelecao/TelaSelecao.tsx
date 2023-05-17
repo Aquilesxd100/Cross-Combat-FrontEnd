54416f51020d7cc6ebd7ea9b26a47836be774811
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-05-17 16:35:25 -0300

    adicao de cursores personalizados

--- src/pages/telaSelecao/TelaSelecao.tsx
+++ src/pages/telaSelecao/TelaSelecao.tsx
@@ -36,16 +36,16 @@ function TelaSelecao() {
                 <button onClick={(() => { linkHandler('/telainicial') })} className="h-[10vw] w-[10vw] bg-100% mr-[15vw] ml-[2vw] brightness-[0.85] hover:brightness-105" style={{backgroundImage: `url(${IMGbotaoVoltar})`}}></button>
             </header>
             <main className="h-[65%] w-full flex justify-center items-center">
-                <div onClick={(() => { selecaoHandler('disney') })} className="w-[23vw] min-h-[60vh] h-[56vh] mx-1.5 bg-100% relative brightness-[0.7] hover:brightness-[1] hover:scale-[1.02] hover:cursor-pointer" style={{backgroundImage: `url(${IMGCardDisney})`}}>
+                <div onClick={(() => { selecaoHandler('disney') })} className="w-[23vw] min-h-[60vh] h-[56vh] mx-1.5 bg-100% relative brightness-[0.7] hover:brightness-[1] hover:scale-[1.02] cursor-pointer" style={{backgroundImage: `url(${IMGCardDisney})`}}>
                     <h4 className="text-[#99B2FF] text-cardsSEL">Disney</h4>
                 </div>
-                <div onClick={(() => { selecaoHandler('animes') })} className="w-[23vw] min-h-[60vh] h-[56vh] mx-1.5 bg-100% relative brightness-[0.7] hover:brightness-[1] hover:scale-[1.02] hover:cursor-pointer" style={{backgroundImage: `url(${IMGCardAnimes})`}}>
+                <div onClick={(() => { selecaoHandler('animes') })} className="w-[23vw] min-h-[60vh] h-[56vh] mx-1.5 bg-100% relative brightness-[0.7] hover:brightness-[1] hover:scale-[1.02] cursor-pointer" style={{backgroundImage: `url(${IMGCardAnimes})`}}>
                     <h4 className="text-cardsSEL text-[#FFFFFF]">Animes</h4>
                 </div>
-                <div onClick={(() => { selecaoHandler('herois') })} className="w-[23vw] min-h-[60vh] h-[56vh] mx-1.5 bg-100% relative brightness-[0.7] hover:brightness-[1] hover:scale-[1.02] hover:cursor-pointer" style={{backgroundImage: `url(${IMGCardHerois})`}}>
+                <div onClick={(() => { selecaoHandler('herois') })} className="w-[23vw] min-h-[60vh] h-[56vh] mx-1.5 bg-100% relative brightness-[0.7] hover:brightness-[1] hover:scale-[1.02] cursor-pointer" style={{backgroundImage: `url(${IMGCardHerois})`}}>
                     <h4 className="text-cardsSEL heroi-gradiente">Heróis</h4>
                 </div>
-                <div onClick={(() => { selecaoHandler('aleatorio') })} className="w-[23vw] min-h-[60vh] h-[56vh] mx-1.5 bg-100% relative brightness-[0.7] hover:brightness-[1] hover:scale-[1.02] hover:cursor-pointer" style={{backgroundImage: `url(${IMGCardMisturado})`}}>
+                <div onClick={(() => { selecaoHandler('aleatorio') })} className="w-[23vw] min-h-[60vh] h-[56vh] mx-1.5 bg-100% relative brightness-[0.7] hover:brightness-[1] hover:scale-[1.02] cursor-pointer" style={{backgroundImage: `url(${IMGCardMisturado})`}}>
                     <h4 className="text-cardsSEL text-[#D6A96B]">Misturado</h4>
                 </div>
             </main>
