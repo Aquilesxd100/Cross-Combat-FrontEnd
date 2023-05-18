89a114cbb58579c44528712cc5d15d07b5435968
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-05-18 10:02:24 -0300

    estilizacao final da base do menu de ajuda

--- src/components/menuAjuda/MenuAjuda.tsx
+++ src/components/menuAjuda/MenuAjuda.tsx
@@ -40,16 +40,16 @@ function MenuAjuda() {
                 <button onClick={(() => { setModalDisplay(modalDisplayDefault); dispatch(setInfosModal(false)) })} className="absolute right-[6.5vw] top-[5.4vw] h-8 w-8 bg-100%" style={{backgroundImage: `url(${IMGBotaoFechar})`, filter: 'drop-shadow(3px 0px 3px rgba(0, 0, 0, 0.3)) drop-shadow(0px 3px 3px rgba(0, 0, 0, 0.3)) drop-shadow(0px -3px 3px rgba(0, 0, 0, 0.3)) drop-shadow(-3px 0px 3px rgba(0, 0, 0, 0.3))'}} />
                 <div className="relative h-[47%] w-[90%]">
                     <img src={IMGPasso1} className="relative mr-1 float-left w-52 h-56 z-[2] drop-shadow-[-3px_-3px_2px_rgba(0,0,0,0.7)]" />
-                    <p className="relative top-[5%] w-[90%] font-[hobostd] text-[1.9vw] text-[#FFA54C]">1. Selecione um atributo de algum dos seus cards.</p>
+                    <p className="relative top-[5%] w-[90%] font-[hobostd] text-[1.9vw] text-[#FFA54C]"><span>1</span> Selecione um atributo de algum dos seus cards.</p>
                     <hr className="absolute top-[46%] left-[40%] h-1 w-[24vw] bg-[#FFA64D] border-[3.8px] border-[#FFA64D] rounded-md" />
                 </div>
                 <div className="relative left-[8%] mt-[-20%] h-[47%] w-[90%]">
                     <img src={IMGPasso2} className="relative ml-2 float-right w-52 h-56 z-[2] drop-shadow-[3px_3px_2px_rgba(0,0,0,0.7)]" />
-                    <p className="relative top-[5%] font-[hobostd] text-[1.9vw] text-[#FFA54C] text-right">2. Escolha um card inimigo para atacar.</p>
+                    <p className="relative top-[5%] font-[hobostd] text-[1.9vw] text-[#FFA54C] text-right"><span>2</span> Escolha um card inimigo para atacar.</p>
                     <hr className="absolute bottom-[22%] right-[40%] h-1 w-[24vw] bg-[#FFA64D] border-[3.8px] border-[#FFA64D] rounded-md" />
                 </div>
                 <div className="relative flex flex-col items-center pt-6 h-[22%] w-[98%]">
-                    <p className="relative font-[hobostd] mb-2 text-[1.9vw] text-[#FFA54C] text-center">3. O card com o maior valor de atributo vence, elimine os três inimigos para ganhar a rodada.</p>
+                    <p className="relative font-[hobostd] mb-2 text-[1.9vw] text-[#FFA54C] text-center"><span>3</span> O card com o maior valor de atributo vence, elimine os três inimigos para ganhar a rodada.</p>
                     <hr className="h-1 w-[90%] bg-[#FFA64D] border-[3.8px] border-[#FFA64D] rounded-md" />
                 </div>
             </div>
