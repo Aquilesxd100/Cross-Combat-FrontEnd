a0d3f3e8de60d23a060eaa4308f4cfee6d632d68
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-05-18 11:56:30 -0300

    fix responsividade botaoGerarCards

--- src/components/botaoGerarCards/BotaoGerarCards.tsx
+++ src/components/botaoGerarCards/BotaoGerarCards.tsx
@@ -31,7 +31,7 @@ function BotaoGerarCards(props : BotaoGerarCardsPropsType) {
     return (
         <div className="w-full h-full flex items-center justify-center">
             <button onClick={(() => { gerarCards() })} className="min-w-[35vw] py-[1.8vw] bg-100% flex justify-center brightness-[0.85] hover:brightness-105" style={{ backgroundImage : `url(${IMGtituloMenu})`}}>
-                <h3 className="font-light text-[calc(2vw+22px)] text-[#FFA64D]">{props.texto}</h3>
+                <h3 className="font-light text-[calc(2vw+22px)] text-[#FFA64D] px-[20px]">{props.texto}</h3>
             </button>
         </div>
     )
