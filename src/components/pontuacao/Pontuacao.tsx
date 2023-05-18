915413155325b4507bf2f367621a9add60c5de31
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-05-18 10:42:48 -0300

    retoque top da pontuacao

--- src/components/pontuacao/Pontuacao.tsx
+++ src/components/pontuacao/Pontuacao.tsx
@@ -5,7 +5,7 @@ import IMGPontuacao from "../../resources/images/display-pontos.png";
 function Pontuacao() {
     const { pontosJogador } = useSelector((state : RootState) => state.pontuacao);
     return (
-        <div className="fixed flex justify-center items-center right-0 w-[12.5%] h-[calc(18vh+7px)] bg-100% top-[41%] z-10" style={{backgroundImage: `url(${IMGPontuacao})`}}>
+        <div className="fixed flex justify-center items-center right-0 w-[12.5%] h-[calc(18vh+7px)] bg-100% top-[40%] z-10" style={{backgroundImage: `url(${IMGPontuacao})`}}>
             <h2 className="relative text-[#60FF2B]  right-[-0.4vw] font-[hobostd] text-[calc(5vw+7px)] font-bold" style={{filter: 'drop-shadow(0px 3px 3px rgba(0, 0, 0, 0.55)) drop-shadow(0px -3px 3px rgba(0, 0, 0, 0.55)) drop-shadow(-3px 0px 3px rgba(0, 0, 0, 0.55))'}}>{pontosJogador}</h2>
         </div>
     );
