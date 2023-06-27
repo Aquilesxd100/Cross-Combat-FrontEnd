55c285dce33cf941e21076e1ab3979a0b1665e84
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-27 17:06:25 -0300

    fix bugs + retoques

--- src/components/menuVitória&Derrota/MenuVitoriaDerrota.tsx
+++ src/components/menuVitória&Derrota/MenuVitoriaDerrota.tsx
@@ -16,7 +16,6 @@ function MenuVitoriaDerrota() {
     const { modalVitoriaActive, modalDerrotaActive } = useSelector((state : RootState) => state.modalStatus);
     const { cardsPreLoadingState } = useSelector((state : RootState) => state.loadingScreen);
     const [modalLogo, setModalLogo] = useState(IMGLogoDerrota);
-    const divModalMenu : any = useRef();
     const textoConfirmar : any = useRef();
     const botaoConfirmar : any = useRef();
     const navigate = useNavigate();
@@ -63,32 +62,36 @@ function MenuVitoriaDerrota() {
     const modalDisplayDefault : any = {
         opacity: 0,
         pointerEvents: "none",
+        transition: "opacity 0.35s"
     };
     const [ modalDisplay, setModalDisplay ] = useState(modalDisplayDefault);
 
     useEffect(() => {
-        if (modalVitoriaActive || modalVitoriaActive) {
+        if (modalVitoriaActive || modalDerrotaActive) {
             const buttonText : string = modalVitoriaActive 
             ? "CONFIRMAR" : "TELA INICIAL";
+
+            textoConfirmar.current.innerText = buttonText;
 
             setModalLogo(modalVitoriaActive 
             ? IMGLogoVitoria : IMGLogoDerrota);
 
-            textoConfirmar.current.innerText = buttonText;
-
-            setModalDisplay({
-                opacity: 1,
-                pointerEvents: "auto"
-            });
+                setTimeout(() => {
+                    setModalDisplay({
+                        opacity: 1,
+                        pointerEvents: "auto",
+                        transition: "opacity 0.35s"
+                    });
+                }, 500)
         } else {
             setModalDisplay(modalDisplayDefault);
         };
-    }, [modalVitoriaActive, modalVitoriaActive]);
+    }, [modalVitoriaActive, modalDerrotaActive]);
 
     return (
-        <div ref={divModalMenu} className="fixed flex items-center justify-center top-0 left-0 h-full w-full bg-black/50 z-10 backdrop-blur-[1.5px]" style={modalDisplay}>
+        <div className="fixed flex items-center justify-center top-0 left-0 h-full w-full bg-black/50 z-10 backdrop-blur-[1.5px]" style={modalDisplay}>
             <div className="relative flex flex-col items-center justify-center w-[calc(31vw+100px)] h-[calc(26vw+100px)] bg-100%" style={{backgroundImage:`url(${IMGModalMenuMaior})`}}>
-                <img src={modalLogo} className="absolute w-[70%] h-[85%] top-[-31%] sombra-padrao-img" />
+                <img src={modalLogo} className="absolute w-[75%] h-[85%] top-[-31%] sombra-padrao-img" />
                 <button ref={botaoConfirmar} className="min-w-[28%] w-[59%] h-[21%] bg-100% bg-no-repeat my-2 text-[3vw] font-bold brightness-[0.85] hover:brightness-110 top-[60%] absolute" style={{backgroundImage: `url(${botaoPadrao})`}} onClick={(() => { confirmButtonHandler(); })}><h3 className="gradiente-laranja" ref={textoConfirmar}>TELA INICIAL</h3></button>
             </div>
         </div>
