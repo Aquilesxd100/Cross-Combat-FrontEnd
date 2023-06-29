aee49cab0f52b79cd6c21a8bba7aa36abdc189fc
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-29 17:44:49 -0300

    adicao de state de cristais ao slice de pontuacao

--- src/redux/slices/pontuacaoSlice.ts
+++ src/redux/slices/pontuacaoSlice.ts
@@ -2,7 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 import { PontuacaoStoreType } from "../../types/types";
 
 const initialState : PontuacaoStoreType = {
-    pontosJogador: 0
+    pontosJogador: 0,
+    quantidadeCristais: 0
 };
 
 const pontuacaoSlice = createSlice({
@@ -14,9 +15,15 @@ const pontuacaoSlice = createSlice({
         },
         setPontuacao: (state, action) => {
             state.pontosJogador = action.payload;
+        },
+        aumentarCristais: (state) => {
+            state.quantidadeCristais += 1;
+        },
+        setCristais: (state, action) => {
+            state.quantidadeCristais = action.payload;
         }
     }
 });
 
-export const { aumentarPontuacao, setPontuacao } = pontuacaoSlice.actions;
+export const { aumentarPontuacao, setPontuacao, aumentarCristais, setCristais } = pontuacaoSlice.actions;
 export default pontuacaoSlice.reducer;
